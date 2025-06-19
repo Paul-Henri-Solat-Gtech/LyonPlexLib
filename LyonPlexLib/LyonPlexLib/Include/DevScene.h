83c48f2496631b7468a4650d9d299fbdfb9e2330
Paulhenrisolat <psolat@gaming.tech>
2025-06-19 10:02:49 +0200

    devscen in progress

--- LyonPlexLib/LyonPlexLib/Include/DevScene.h
+++ LyonPlexLib/LyonPlexLib/Include/DevScene.h
@@ -1,5 +1,12 @@
 #pragma once
-class DevScene
+class DevScene : public Scene
 {
+public:
+	void Start() override;
+	void Update(float deltatime) override;
+	void Release() override;
+
+private:
+
 };
 
