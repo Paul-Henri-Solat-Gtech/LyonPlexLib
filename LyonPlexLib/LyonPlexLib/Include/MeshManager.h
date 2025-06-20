964fdf55a089021e04d4fae0e647d8cfcaf80da1
Clement-Trt <ctouratier@gaming.tech>
2025-06-20 12:46:52 +0200

    clean & gitignore update

--- LyonPlexLib/LyonPlexLib/Include/MeshManager.h
+++ LyonPlexLib/LyonPlexLib/Include/MeshManager.h
@@ -1,46 +1,5 @@
 ﻿#pragma once
 
-//#include "GraphicsDevice.h"
-//#include "ResourceManager.h"
-//
-//struct VertexParam
-//{
-//	XMFLOAT3 Position;		// Position des points
-//	XMFLOAT4 Color;			// Couleur des points
-//	XMFLOAT2 TexCoord;	// Coordonees du point sur la texture ?
-//	//XMFLOAT3 Normal;		// Normale ・la face visible du mesh (vecteur de direction)
-//};
-//
-//
-//struct MeshData
-//{
-//	std::vector<VertexParam> vertices;   // ici, on stocke directement les VertexParam (pas des pointeurs pcq struct legere)
-//	std::vector<uint16_t> indices;
-//	UINT vOffset = 0;   // decalage dans le buffer global
-//	UINT vSize = 0;   // nombre de sommets
-//	UINT iOffset = 0;   // decalage dans le buffer global
-//	UINT iSize = 0;   // nombre d’indices
-//
-//	bool LoadFromFile(const std::string& path)
-//	{
-//		// ici tu lis le fichier (obj, fbx, gltf, etc.),
-//		// tu fills `vertices` et `indices`,
-//		// et tu retournes true/false suivant le succes.
-//	}
-//	void Unload()
-//	{
-//		// si tu veux desallouer ou liberer toute ressource GPU associee
-//		vertices.clear();
-//		indices.clear();
-//	}
-//};
-//
-//class MeshManager
-//{
-//public:
-//	void Init(GraphicsDevice* graphicsDevice);
-
-
 #include "GraphicsDevice.h"
 #include "ResourceManager.h"
 //#include "d3dx12.h"
