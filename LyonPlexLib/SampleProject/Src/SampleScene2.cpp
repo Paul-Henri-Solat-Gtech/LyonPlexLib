76763891342ee5c496f74e19a84dfdeb477bc402
Clement-Trt <ctouratier@gaming.tech>
2025-06-20 12:00:42 +0200

    Mesh3D + small fixes

--- LyonPlexLib/SampleProject/Src/SampleScene2.cpp
+++ LyonPlexLib/SampleProject/Src/SampleScene2.cpp
@@ -28,29 +28,30 @@ void SampleScene2::Start()
 	CreateEntity("cube2");
 	//AddComponent<Type_2D5>("cube2", new Type_2D5());
 	AddComponent<Type_3D>("cube2", new Type_3D());
-	AddComponent<MeshComponent>("cube2", new MeshComponent(3, -1));
-	GetComponent<TransformComponent>("cube2")->position = { 0, 0, 1 };
+	AddComponent<MeshComponent>("cube2", new MeshComponent(3, 0));
+	GetComponent<TransformComponent>("cube2")->position = { 0, 0, 0 };
+	GetComponent<TransformComponent>("cube2")->scale = { 2, 2, 2 };
 
 	CreateEntity("bras");
 	AddComponent<Type_2D>("bras", new Type_2D());
 	AddComponent<MeshComponent>("bras", new MeshComponent(2, 2));
-	GetComponent<TransformComponent>("bras")->position = { 400, 400, 0 };
-	GetComponent<TransformComponent>("bras")->scale = { 1600, 1000, 0 };
+	GetComponent<TransformComponent>("bras")->position = { 400, 500, 0 };
+	GetComponent<TransformComponent>("bras")->scale = { 800, 500, 0 };
 	GetComponent<TransformComponent>("bras")->SetRotation(0, 0, 180);
 	GetComponent<TransformComponent>("bras")->dirty = true;
 
 	CreateEntity("cube2D");
 	AddComponent<Type_2D>("cube2D", new Type_2D());
 	AddComponent<MeshComponent>("cube2D", new MeshComponent(2, 0));
-	GetComponent<TransformComponent>("cube2D")->position = { 200, 200, 0 };
-	GetComponent<TransformComponent>("cube2D")->scale = { 50, 50, 0 };
+	GetComponent<TransformComponent>("cube2D")->position = { 400, 300, 0 };
+	GetComponent<TransformComponent>("cube2D")->scale = { 80, 60, 0 };
 	//GetComponent<TransformComponent>("cube2D")->SetRotation(0 ,0, 0);
 	GetComponent<TransformComponent>("cube2D")->dirty = true;
 
 	CreateEntity("cube4");
 	AddComponent<Type_3D>("cube4", new Type_3D());
 	AddComponent<MeshComponent>("cube4", new MeshComponent(2, 0));
-	GetComponent<TransformComponent>("cube4")->position = { 1, 0, 3 };
+	GetComponent<TransformComponent>("cube4")->position = { 0, 0, 0 };
 
 	//GetComponent<TransformComponent>("camera")->parent = { GetEntity("cube")->id };
 }
@@ -64,44 +65,44 @@ void SampleScene2::Update(float deltatime)
 	if (InputManager::GetKeyIsPressed('Z'))
 	{
 		//OutputDebugStringA("\nZ is pressed ! \n");
-		GetComponent<TransformComponent>("cube")->position.z += 1.f * deltatime;
-		GetComponent<TransformComponent>("cube")->dirty = true;
-		GetComponent<TransformComponent>("camera")->position.z += 1.f * deltatime;
+		//GetComponent<TransformComponent>("cube")->position.z += 10.f * deltatime;
+		//GetComponent<TransformComponent>("cube")->dirty = true;
+		GetComponent<TransformComponent>("camera")->position.z += 10.f * deltatime;
 		GetComponent<TransformComponent>("camera")->dirty = true;
 	}
 	if (InputManager::GetKeyIsPressed('S'))
 	{
-		GetComponent<TransformComponent>("cube")->position.z -= 1.f * deltatime;
-		GetComponent<TransformComponent>("cube")->dirty = true;
-		GetComponent<TransformComponent>("camera")->position.z -= 1.f * deltatime;
+		//GetComponent<TransformComponent>("cube")->position.z -= 10.f * deltatime;
+		//GetComponent<TransformComponent>("cube")->dirty = true;
+		GetComponent<TransformComponent>("camera")->position.z -= 10.f * deltatime;
 		GetComponent<TransformComponent>("camera")->dirty = true;
 	}
 	if (InputManager::GetKeyIsPressed('Q'))
 	{
-		GetComponent<TransformComponent>("cube")->position.x -= 1.f * deltatime;
-		GetComponent<TransformComponent>("cube")->dirty = true;
-		GetComponent<TransformComponent>("camera")->position.x -= 1.f * deltatime;
+		//GetComponent<TransformComponent>("cube")->position.x -= 10.f * deltatime;
+		//GetComponent<TransformComponent>("cube")->dirty = true;
+		GetComponent<TransformComponent>("camera")->position.x -= 10.f * deltatime;
 		GetComponent<TransformComponent>("camera")->dirty = true;
 	}
 	if (InputManager::GetKeyIsPressed('D'))
 	{
-		GetComponent<TransformComponent>("cube")->position.x += 1.f * deltatime;
-		GetComponent<TransformComponent>("cube")->dirty = true;
-		GetComponent<TransformComponent>("camera")->position.x += 1.f * deltatime;
+		//GetComponent<TransformComponent>("cube")->position.x += 10.f * deltatime;
+		//GetComponent<TransformComponent>("cube")->dirty = true;
+		GetComponent<TransformComponent>("camera")->position.x += 10.f * deltatime;
 		GetComponent<TransformComponent>("camera")->dirty = true;
 	}
 	if (InputManager::GetKeyIsPressed(VK_SPACE))
 	{
-		GetComponent<TransformComponent>("cube")->position.y += 1.f * deltatime;
-		GetComponent<TransformComponent>("cube")->dirty = true;
-		GetComponent<TransformComponent>("camera")->position.y += 1.f * deltatime;
+		//GetComponent<TransformComponent>("cube")->position.y += 10.f * deltatime;
+		//GetComponent<TransformComponent>("cube")->dirty = true;
+		GetComponent<TransformComponent>("camera")->position.y += 10.f * deltatime;
 		GetComponent<TransformComponent>("camera")->dirty = true;
 	}
 	if (InputManager::GetKeyIsPressed(VK_CONTROL))
 	{
-		GetComponent<TransformComponent>("cube")->position.y -= 1.f * deltatime;
-		GetComponent<TransformComponent>("cube")->dirty = true;
-		GetComponent<TransformComponent>("camera")->position.y -= 1.f * deltatime;
+		//GetComponent<TransformComponent>("cube")->position.y -= 10.f * deltatime;
+		//GetComponent<TransformComponent>("cube")->dirty = true;
+		GetComponent<TransformComponent>("camera")->position.y -= 10.f * deltatime;
 		GetComponent<TransformComponent>("camera")->dirty = true;
 
 	}
