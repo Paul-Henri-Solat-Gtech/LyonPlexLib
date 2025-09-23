be535f2d3ceb7ebceb88ea18ec4c5043db61e89f
Paulhenrisolat <psolat@gaming.tech>
2025-09-24 01:44:40 +0200

    Update GameScene.h

--- LyonPlexLib/SampleProject/Include/GameScene.h
+++ LyonPlexLib/SampleProject/Include/GameScene.h
@@ -5,6 +5,12 @@
 #include "Portals.h"
 #include "FpsCamera.h"
 
+struct portal 
+{
+	bool hasSpawned = false;
+	bool isFinished = false;
+};
+
 class GameScene : public Scene
 {
 public:
@@ -35,7 +41,7 @@ private:
 	bool m_youWin;
 	float m_chronoChangeScene;
 
-	//Portals for the lvl1
+	//Portals for the lvl1 -> utiliser la struct
 	Portals* m_portal = nullptr;
 	bool m_p1Spawned, m_p2Spawned, m_p3Spawned, m_p4Spawned, m_p5Spawned, m_pBossSpawned;
 	bool m_p1Finished, m_p2Finished, m_p3Finished, m_p4Finished, m_p5Finished, m_pBossFinished;
